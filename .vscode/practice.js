525899996d9f3257bde889259d7027b18fa9312a
bobbyvarma <bobby.g@paradigmit.com>
2023-05-04 17:41:16 +0530

    02/12

--- .vscode/practice.js
+++ .vscode/practice.js
@@ -1,32 +1,6 @@
-function main() {
-  console.log("working");
-  let result = "";
-  for (let x = 0; x <= 9; x++) {
-    for (let y = 0; y <= x; y++) {
-      result += "#";
-    }
-    result += "\n";
-  }
-  console.log(result);
-
-  console.log("block and const example");
-
-//   const x = 2; // Allowed
-
-  {
-    const x = 3; // Allowed
-    console.log(`i'm in side of 1 block : ${x}`);
-  }
-
-  {
-    const x = 4; // Allowed
-    console.log(`i'm oinut side of 2 block : ${x}`);
-  }
-  {
-    console.log(`i'm in side of 3 block : ${x}`);
-  }
-
-  console.log(`i'm out side of blocks : ${x}`);
+function main(){
+   var name="bobby";
+   name.slice(0,3);
+ 
 }
-
-main();
+main()
