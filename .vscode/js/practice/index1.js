0a2650c1004f44ee540f375286ee0e891c9de215
bobbyvarma <bobby.g@paradigmit.com>
2023-05-17 17:55:59 +0530

    17/5

---
+++ .vscode/js/practice/index1.js
@@ -0,0 +1,9 @@
+function functionName(...params) {
+  return null;
+}
+
+const functionName2 = (...params) => null;
+
+const addNumbers = (...params) => params.reduce(add, 0);
+
+const add = (a, b) => a + b;
