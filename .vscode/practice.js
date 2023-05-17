db816993e69f9ea99c31b6b3e6e4331aa103f5ab
bobbyvarma <bobby.g@paradigmit.com>
2023-05-17 17:55:09 +0530

    17-05

--- .vscode/practice.js
+++ .vscode/practice.js
@@ -1,6 +1,14 @@
-function main(){
-   var name="bobby";
-   name.slice(0,3);
- 
+const input = prompt("pick the number :");
+if (!isNaN(input)) {
+  if (count % 3 == 0 && count % 5 == 0) {
+    console.log("fizzbuzz" + " ");
+  } else if (count % 3 == 0) {
+    console.log("fizz" + " ");
+  } else if (count % 5 == 0) {
+    console.log("buzz" + " ");
+  } else {
+    console.log("number");
+  }
+} else {
+  console.log("please select the number :");
 }
-main()
